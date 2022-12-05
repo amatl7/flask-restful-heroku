e6d8e6faeed84588705b53e76830899b37c0ac55
amatl7 <anthonymatl2001@gmail.com>
2022-12-05 11:39:45 -0600

    Fix queries

--- main.py
+++ main.py
@@ -1,96 +1,100 @@
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
 from flask_cors import CORS
+import psycopg2
+connection = psycopg2.connect(host='csce-315-db.engr.tamu.edu', database='csce315_912_11', user='csce315_912_matl', password='1')
+cursor = connection.cursor()
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
+class executeQuery (Resource):
+    def get(self, queryStr):
+        try:
+            cursor.execute(queryStr)
+            myArr = []
+            myStr = ""
 
-import datetime
-import random
-import sys
-import time
-
-import psycopg2
-from flask import Flask
-from flask import json
-
-connection = psycopg2.connect(host='csce-315-db.engr.tamu.edu', database='csce315_912_11', user='csce315_912_matl', password='1')
-cursor = connection.cursor()
-
-  
-x = datetime.datetime.now()
-  
-# Initializing flask app
-app = Flask(__name__)
-
-   
-# Route for seeing a data
-@app.route('/data/<queryStr>')
-def get_time(queryStr):
-    
-    cursor.execute(queryStr)
-    myArr = []
-    myStr = ""
-
-    for query in cursor:
-        myArr.append(str(query))
-        myStr += str(query) + '.'
-    # Returning an api for showing in  reactjs
-    return {
-        'QueryResult':myArr
-        }
+            for query in cursor:
+                myArr.append(str(query))
+                myStr += str(query) + '.'
+            return {'QueryResult':myArr}
+        except:
+            return []
 
 
-  
-@app.route('/data/itemtable')
-def get_itemtable():
-    cursor.execute('SELECT * FROM itemtable')
-    myArr = []
+api.add_resource(executeQuery, '/data/<queryStr>')
 
-    for query in cursor:
-        myArr.append(str(query))
-    # Returning an api for showing in  reactjs
-    return {
-        'QueryResult':myArr
-        }
+class getItemTable (Resource):
+    def get(self):
+        try:
+            cursor.execute('SELECT * FROM itemtable')
+            myArr = []
 
-@app.route('/data/menutable')
-def get_menutable():
-    cursor.execute('SELECT * FROM menutable')
-    myArr = []
+            for query in cursor:
+                myArr.append(str(query))
+            # Returning an api for showing in  reactjs
+            return {
+                'QueryResult':myArr
+                }
+        except:
+            return []
 
-    for query in cursor:
-        myArr.append(str(query))
-    # Returning an api for showing in  reactjs
-    return {
-        'QueryResult':myArr
-        }
 
-@app.route('/data/ordertable')
-def get_ordertable():
-    cursor.execute('SELECT * FROM ordertable')
-    myArr = []
+api.add_resource(getItemTable, '/data/itemtable')
 
-    for query in cursor:
-        myArr.append(str(query))
-    # Returning an api for showing in  reactjs
-    return {
-        'QueryResult':myArr
-        }
+class getMenuTable (Resource):
+    def get(self):
+        try:
+            cursor.execute('SELECT * FROM menutable')
+            myArr = []
 
-@app.route('/result/<queryStr>')
-def run_update(queryStr):
-    
-    cursor.execute(queryStr)
+            for query in cursor:
+                myArr.append(str(query))
+            # Returning an api for showing in  reactjs
+            return {
+                'QueryResult':myArr
+                }
+        except:
+            return []
 
-    # Returning an api for showing in  reactjs
-    connection.commit()
-    return {
-        'QueryResult':'Success'
-        }
 
+api.add_resource(getMenuTable, '/data/menutable')
+
+class getOrderTable (Resource):
+    def get(self):
+        try:
+            cursor.execute('SELECT * FROM ordertable')
+            myArr = []
+
+            for query in cursor:
+                myArr.append(str(query))
+            # Returning an api for showing in  reactjs
+            return {
+                'QueryResult':myArr
+                }
+        except:
+            return []
+
+
+api.add_resource(getOrderTable, '/data/ordertable')
+
+class runUpdate (Resource):
+    def get(self, queryStr):
+        try:
+            cursor.execute(queryStr)
+
+            # Returning an api for showing in  reactjs
+            connection.commit()
+            return {
+                'QueryResult':'Success'
+                }
+        except:
+            return 'Query failed'
+
+
+api.add_resource(runUpdate, '/result/<queryStr>')
 
 if __name__ == '__main__':
     app.run()

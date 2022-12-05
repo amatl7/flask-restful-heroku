34cb3dad4c23905a537bed7c89f57ea34ad66170
amatl7 <anthonymatl2001@gmail.com>
2022-12-05 11:11:19 -0600

    Add code for our API

--- main.py
+++ main.py
@@ -7,21 +7,90 @@ api = Api(app)
 CORS(app)
 
 
-class status (Resource):
-    def get(self):
-        try:
-            return {'data': 'Api is Running'}
-        except:
-            return {'data': 'An Error Occurred during fetching Api'}
+import datetime
+import random
+import sys
+import time
+
+import psycopg2
+from flask import Flask
+from flask import json
+
+connection = psycopg2.connect(host='csce-315-db.engr.tamu.edu', database='csce315_912_11', user='csce315_912_matl', password='1')
+cursor = connection.cursor()
+
+  
+x = datetime.datetime.now()
+  
+# Initializing flask app
+app = Flask(__name__)
+
+   
+# Route for seeing a data
+@app.route('/data/<queryStr>')
+def get_time(queryStr):
+    
+    cursor.execute(queryStr)
+    myArr = []
+    myStr = ""
+
+    for query in cursor:
+        myArr.append(str(query))
+        myStr += str(query) + '.'
+    # Returning an api for showing in  reactjs
+    return {
+        'QueryResult':myArr
+        }
 
 
-class Sum(Resource):
-    def get(self, a, b):
-        return jsonify({'data': a+b})
+  
+@app.route('/data/itemtable')
+def get_itemtable():
+    cursor.execute('SELECT * FROM itemtable')
+    myArr = []
 
+    for query in cursor:
+        myArr.append(str(query))
+    # Returning an api for showing in  reactjs
+    return {
+        'QueryResult':myArr
+        }
 
-api.add_resource(status, '/')
-api.add_resource(Sum, '/add/<int:a>,<int:b>')
+@app.route('/data/menutable')
+def get_menutable():
+    cursor.execute('SELECT * FROM menutable')
+    myArr = []
+
+    for query in cursor:
+        myArr.append(str(query))
+    # Returning an api for showing in  reactjs
+    return {
+        'QueryResult':myArr
+        }
+
+@app.route('/data/ordertable')
+def get_ordertable():
+    cursor.execute('SELECT * FROM ordertable')
+    myArr = []
+
+    for query in cursor:
+        myArr.append(str(query))
+    # Returning an api for showing in  reactjs
+    return {
+        'QueryResult':myArr
+        }
+
+@app.route('/result/<queryStr>')
+def run_update(queryStr):
+    
+    cursor.execute(queryStr)
+
+    # Returning an api for showing in  reactjs
+    connection.commit()
+    return {
+        'QueryResult':'Success'
+        }
+
 
 if __name__ == '__main__':
     app.run()
